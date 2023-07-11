1bdaddc37fab877a89caf95d127e7e447e853894
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-07-10 22:35:53 -0400

    Add files via upload

---
+++ client/src/imports.js
@@ -0,0 +1,7 @@
+export {default as Header} from './components/Header';
+export {default as Home} from './components/Home';
+export {default as Partners} from './components/Partners';
+export {default as About} from './components/About';
+export {default as Footer} from './components/Footer';
+export {default as Contact} from './components/Contact';
+export {default as PartnerDetails} from './components/PartnerDetails'
