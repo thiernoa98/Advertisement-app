8b29d29430d5ae79062a6872461efa91e77a89c7
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-07-16 22:23:22 -0400

    Add files via upload

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/css/App.css'
+import logo from '../assets/img/logo.png'
+function Footer() {
+  return (
+    <div className='footer'>
+      <img className='logo' src={logo} alt="logo" />
+      @Nile Add 2023
+    </div>
+  )
+}
+
+export default Footer
