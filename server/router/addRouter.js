63e9681532833872e7452c76b80b99557a0625e5
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-07-24 23:46:08 -0400

    Update addRouter.js

--- server/router/addRouter.js
+++ server/router/addRouter.js
@@ -3,30 +3,8 @@ const router = express();
 
 const {sendEmail, confirmationEmail, getAllPartners, getOnePartner} = require('../controller/addController');
 
-//creating a new add order
-//localhost:5000/api/placeOrder
-// router.post('/placeOrder', placeOrder);
-
-
-//sendEmail
-//localhost:5000/api/getEmail
-// router.get('/getEmail', (req, res)=>
-// sendEmail()
-// .then((response)=> res.send(response.message))
-// .catch((e)=> res.status(500).send(e.message))
-// );
-
-//post api
-//localhost:5000/api/sendEmail
-// router.post('/sendEmail', (req,res)=>{
-//     sendEmail(req.body)
-//     .then((response)=> res.send(response.message))
-//     .catch((e)=> res.status(500).send(e.message))
-// });
 router.post('/sendEmail', sendEmail);
 
-
-//localhost:5000/api/confirm
 //get notiffication
 router.post('/confirm', confirmationEmail);
 
